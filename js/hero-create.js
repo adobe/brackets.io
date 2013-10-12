db15aa18b3504f760f716febbfb5a5229b2399a4
adrocknaphobia <adrocknaphobia@gmail.com>
2013-10-12 00:14:54 -0400

    prototype create js hero animation.

---
+++ js/hero-create.js
@@ -0,0 +1,63 @@
+(function (lib, img, cjs) {
+
+var p; // shortcut to reference prototypes
+
+// stage content:
+(lib.Untitled1 = function(mode,startPosition,loop) {
+if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
+
+	// tag highlight
+	this.instance = new lib.fragmenttagunhighlight();
+	this.instance.setTransform(346,331);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance,p:{x:346}}]}).to({state:[{t:this.instance,p:{x:-345.9}}]},24).to({state:[{t:this.instance,p:{x:346}}]},160).wait(26));
+
+	// html code
+	this.instance_1 = new lib.fragmenthtmlcode();
+	this.instance_1.setTransform(144,349);
+
+	this.instance_2 = new lib.Symbol1();
+	this.instance_2.setTransform(672,525.5,1,1,0,0,0,528,176.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1}]}).to({state:[{t:this.instance_2,p:{y:525.5}}]},29).to({state:[{t:this.instance_2,p:{y:539.1}}]},1).to({state:[{t:this.instance_2,p:{y:552.9}}]},1).to({state:[{t:this.instance_2,p:{y:566.8}}]},1).to({state:[{t:this.instance_2,p:{y:581}}]},1).to({state:[{t:this.instance_2,p:{y:595.3}}]},1).to({state:[{t:this.instance_2,p:{y:609.8}}]},1).to({state:[{t:this.instance_2,p:{y:624.6}}]},1).to({state:[{t:this.instance_2,p:{y:639.5}}]},1).to({state:[{t:this.instance_2,p:{y:654.6}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:660.5}}]},1).to({state:[{t:this.instance_2,p:{y:646.6}}]},1).to({state:[{t:this.instance_2,p:{y:632}}]},1).to({state:[{t:this.instance_2,p:{y:616.5}}]},1).to({state:[{t:this.instance_2,p:{y:600.3}}]},1).to({state:[{t:this.instance_2,p:{y:583.4}}]},1).to({state:[{t:this.instance_2,p:{y:565.6}}]},1).to({state:[{t:this.instance_2,p:{y:547.1}}]},1).to({state:[{t:this.instance_2,p:{y:527.8}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).to({state:[{t:this.instance_2,p:{y:525.5}}]},1).wait(1));
+
+	// background
+	this.instance_3 = new lib.hero03quickedit();
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_3}]}).to({state:[{t:this.instance_3}]},209).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,1200,704);
+
+
+// symbols:
+(lib.fragmenthtmlcode = function() {
+	this.initialize(img.fragmenthtmlcode);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1056,353);
+
+
+(lib.fragmenttagunhighlight = function() {
+	this.initialize(img.fragmenttagunhighlight);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,245,20);
+
+
+(lib.hero03quickedit = function() {
+	this.initialize(img.hero03quickedit);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1200,704);
+
+
+(lib.Symbol1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.fragmenthtmlcode();
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,1056,353);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{});
+var lib, images, createjs;
