8705d774dab2c0a59ffffa7412d8df863da1c1a4
Lawrence Hsu <larzshinobi@gmail.com>
2013-10-23 16:26:22 -0700

    Created files for Download page.

---
+++ _download/js/nav.js
@@ -0,0 +1,22 @@
+$('#hamburger').on('click touchstart', function() {
+   $('.nav').show();
+   return false;
+});
+
+$('html').on('click touchstart', function() {
+    if ($(window).width() <= 890) {
+        $('.nav').hide();
+    } else {
+        $('.nav').show();
+    }
+})
+
+$(window).bind('resize', function(){
+    if ($(window).width() > 890) {
+        $('.nav').show();
+    }
+});
+
+$('.nav').on('click touchstart', function(e) {
+    e.stopPropagation();
+});
